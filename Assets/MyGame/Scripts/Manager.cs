6e41cf81095e28ee7f9d852566528ceb9eb593e0
mduernberger <melanie.duernberger05@htl-salzburg.ac.at>
2022-05-12 10:45:19 +0200

    added scriptable name object

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 //Hilfsstruktur Aufzählung
 public enum MyScenes
@@ -15,9 +17,35 @@ public enum MyScenes
 
 public class Manager : MonoBehaviour
 {
-    //Aufruf in Inspector OnClick bei Button, generisch über Parameter
- public void SwitchTheScene(int x)
+
+    public TMP_InputField ipfKidsName;
+    public TMP_Text displayKidsName;
+
+    private SoRuntimeData runtimeData;
+
+    private void Start()
     {
+        runtimeData = Resources.Load<SoRuntimeData>("KinderaddiererRuntimeData");
+
+
+        if (SceneManager.GetActiveScene().buildIndex == (int)MyScenes.FirstScene)
+        {
+            displayKidsName.text = runtimeData.nameKid;
+        }
+    }
+
+    //Aufruf in Inspector OnClick bei Button, generisch über Parameter
+    public void SwitchTheScene(int x)
+    {
+        Debug.Log("button pressed, get kidsname " + ipfKidsName.text);
+
+
+        if (SceneManager.GetActiveScene().buildIndex == (int)MyScenes.WelcomeScene)
+        {
+            displayKidsName.text = runtimeData.nameKid;
+        }
+
+        runtimeData.nameKid = ipfKidsName.text;
         SceneManager.LoadScene(x);
     }
 
